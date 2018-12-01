f715c0b310d299120b8305841da0939b99e177f2
Sorin Sbarnea <ssbarnea@redhat.com>
2018-12-01 14:46:49 +0000

    Enable pre-commit linting

--- roles/dsm/templates/mmirror.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p repo/centos/7/os/x86_64
-rsync -ahv rsync://mirror.bytemark.co.uk/centos/7/os/x86_64/  repo/centos/7/os/x86_64
-
-#http://mirror.yandex.ru/centos/7/os/x86_64/ repo/centos/7/os/x86_64
-
