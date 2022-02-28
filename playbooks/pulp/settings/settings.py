64aaaf2e6c5682a38962faab9dfe642f5bfe5f75
Sorin Sbarnea <ssbarnea@redhat.com>
2022-02-28 12:32:32 +0000

    flake8

--- playbooks/pulp/settings/settings.py
+++ playbooks/pulp/settings/settings.py
@@ -1,4 +1,5 @@
+"""Pulp settings."""
 # https://docs.pulpproject.org/settings.html
-CONTENT_ORIGIN="https://pulp.sbarnea.com"
-ANSIBLE_API_HOSTNAME="https://pulp.sbarnea.com"
-ANSIBLE_CONTENT_HOSTNAME="https:/pulp.sbarnea.com/pulp/content"
+CONTENT_ORIGIN = "https://pulp.sbarnea.com"
+ANSIBLE_API_HOSTNAME = "https://pulp.sbarnea.com"
+ANSIBLE_CONTENT_HOSTNAME = "https:/pulp.sbarnea.com/pulp/content"
