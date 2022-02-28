fb343de4bcddad320fb10391e35f262f8d4b5c6c
Sorin Sbarnea <ssbarnea@redhat.com>
2022-02-28 10:20:28 +0000

    Clean historical junk

---
+++ playbooks/pulp/settings/settings.py
@@ -0,0 +1,5 @@
+# https://docs.pulpproject.org/settings.html
+CONTENT_ORIGIN="https://pulp.sbarnea.com"
+ANSIBLE_API_HOSTNAME="https://pulp.sbarnea.com"
+ANSIBLE_CONTENT_HOSTNAME="https:/pulp.sbarnea.com/pulp/content"
+
